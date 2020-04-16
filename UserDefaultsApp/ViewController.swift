69333b697a873f12602682c9cb433f734a913e44
YASIN AKCA <yasin.akca@bil.omu.edu.tr>
2020-04-16 17:29:43 +0300

    Initial Commit

---
+++ UserDefaultsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultsApp
+//
+//  Created by YASIN AKCA on 16.04.2020.
+//  Copyright © 2020 YASIN AKCA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
